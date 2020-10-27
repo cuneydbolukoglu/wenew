ec43b7dd9b3a46da8f5772b3bfb18bc24fe862ee
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2020-10-27 22:53:43 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    console.log("wenew")
+    console.log("wenew");
 
-})
+    var owl = $('.owl-carousel')
+
+    owl.owlCarousel({
+        loop: true,
+        margin: 16,
+        responsiveClass: true,
+        autoWidth: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 1,
+                nav: false,
+                loop: false
+            },
+        }
+    });
+
+});
